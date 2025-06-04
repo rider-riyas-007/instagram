bcd71e5c64a036614bb3b76e416f437c3f510c97
rider-riyas-007 <jjjewishjew@gmail.com>
2025-06-04 13:26:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import ViewStory from './ViewStory.jsx'
-import Profile from './Profile.jsx'
-import Reels from './Reels.jsx'
-import Explore from './Explore.jsx'
-import Search from './Search.jsx'
-const router = createBrowserRouter([
-  {
-   path:'/',
-    element:<App />
-  },
-  {
-    path:'/story/:id/:tot',
-    element:<ViewStory/>
-  },
-  {
-    path:'/profile',
-    element:<Profile/>
-  },
-  {
-    path:'/reels',
-    element:<Reels/>
-  },
-  {
-    path:'/explore',
-    element:<Explore/>
-  },
-  {
-    path:'/search',
-    element:<Search/>
-  }
-])
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
-  
-   <RouterProvider router={router}/>
-  
-)
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
