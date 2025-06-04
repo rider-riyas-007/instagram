53c991ae3a65621bb3c3d1484ac305b28edf3cff
rider-riyas-007 <jjjewishjew@gmail.com>
2025-06-04 13:27:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import Feed from './Feed'
-import Suggestions from './Suggestions'
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import Suggestions from './Suggestions';
+import Profile from './Profile';
+import Reels from './Reels';
+import Explore from './Explore';
+import Search from './Search';
+import ViewStory from './ViewStory';
 
 function App() {
   return (
-    <div className="d-flex  vh-100">
-        <div className='w-20'><Sidebar/></div>
-        <div className='w-50 ' ><Feed/></div>
-        <div className='w-30'><Suggestions/></div>
+    <div className="d-flex vh-100">
+      <div className="w-20"><Sidebar /></div>
+      <div className="w-50">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/reels" element={<Reels />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/story/:id/:tot" element={<ViewStory />} />
+        </Routes>
+      </div>
+      <div className="w-30"><Suggestions /></div>
     </div>
-  )
+  );
 }
-// w-25 w-50 w-75 w-100
-export default App
+
+export default App;
